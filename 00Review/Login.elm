efd03d049c97284eb33c5561f52a54449e642a17
James Moore <james@knowthen.com>
2016-09-12 19:26:02 -0700

    added 00Review

---
+++ 00Review/Login.elm
@@ -0,0 +1,80 @@
+module Login exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Html.App as App
+
+
+-- model
+
+
+type alias Model =
+    { username : String
+    , password : String
+    }
+
+
+initModel : Model
+initModel =
+    { username = ""
+    , password = ""
+    }
+
+
+
+-- update
+
+
+type Msg
+    = UsernameInput String
+    | PasswordInput String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        UsernameInput username ->
+            { model | username = username }
+
+        PasswordInput password ->
+            { model | password = password }
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h3 [] [ text "Login Page... So far" ]
+        , Html.form []
+            [ input
+                [ type' "text"
+                , onInput UsernameInput
+                , placeholder "username"
+                ]
+                []
+            , input
+                [ type' "password"
+                , onInput PasswordInput
+                , placeholder "password"
+                ]
+                []
+            , input [ type' "submit" ]
+                [ text "Login" ]
+            ]
+        , hr [] []
+        , h4 [] [ text "Login Model:" ]
+        , p [] [ text <| toString model ]
+        ]
+
+
+main : Program Never
+main =
+    App.beginnerProgram
+        { model = initModel
+        , view = view
+        , update = update
+        }
