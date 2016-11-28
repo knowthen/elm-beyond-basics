25d373974f1b2d47dd04ad3208068ec6f5e288cb
James Moore <james@knowthen.com>
2016-11-28 07:38:33 -0800

    add 14RLBWebsocket at 0.18

---
+++ 14RLBWebsocket/completed/src/LeaderBoard.elm
@@ -0,0 +1,251 @@
+module LeaderBoard exposing (..)
+
+import Html exposing (..)
+import Html.Events exposing (..)
+import Html.Attributes exposing (..)
+import Json.Encode as JE
+import Json.Decode as JD
+import Json.Decode.Pipeline as JDP
+import WebSocket exposing (..)
+
+
+-- model
+
+
+type alias Model =
+    { error : Maybe String
+    , query : String
+    , runners : List Runner
+    , active : Bool
+    }
+
+
+type alias Runner =
+    { id : String
+    , name : String
+    , location : String
+    , age : Int
+    , bib : Int
+    , estimatedDistance : Float
+    , lastMarkerDistance : Float
+    , lastMarkerTime : Float
+    , pace : Float
+    }
+
+
+
+-- tempRunners : List Runner
+-- tempRunners =
+--     [ Runner "1" "James Moore" "Turlock CA" 42 1234 0 1 1463154945381 0.125
+--     , Runner "2" "Meb Keflezighi" "Turlock CA" 41 1238 0 1 1463154945381 0.09
+--     ]
+
+
+type alias RunnerWsMsg =
+    { name : String
+    , runner : Runner
+    }
+
+
+initModel : Model
+initModel =
+    { error = Nothing
+    , query = ""
+    , runners = []
+    , active = False
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initModel, listenRunnersCmd )
+
+
+url : String
+url =
+    "ws://localhost:5000/runners"
+
+
+listenRunnersCmd : Cmd Msg
+listenRunnersCmd =
+    send url (encodeMsg "listen runners" JE.null)
+
+
+encodeMsg : String -> JE.Value -> String
+encodeMsg name data =
+    JE.object
+        [ ( "name", JE.string name )
+        , ( "data", data )
+        ]
+        |> JE.encode 0
+
+
+
+-- update
+
+
+type Msg
+    = SearchInput String
+    | Search
+    | WsMessage String
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SearchInput query ->
+            ( { model | query = query }, Cmd.none )
+
+        Search ->
+            ( model, Cmd.none )
+
+        WsMessage wsMsg ->
+            wsMessage wsMsg model
+
+
+wsMessage : String -> Model -> ( Model, Cmd Msg )
+wsMessage wsMsg model =
+    case JD.decodeString msgDecoder wsMsg of
+        Ok { name, runner } ->
+            case name of
+                "new runner" ->
+                    ( { model
+                        | runners = runner :: model.runners
+                      }
+                    , Cmd.none
+                    )
+
+                "update runner" ->
+                    let
+                        updatedRunners =
+                            List.map
+                                (\r ->
+                                    if r.id == runner.id then
+                                        runner
+                                    else
+                                        r
+                                )
+                                model.runners
+                    in
+                        ( { model
+                            | runners = updatedRunners
+                          }
+                        , Cmd.none
+                        )
+
+                _ ->
+                    ( { model
+                        | error = Just ("Unrecognized Message: " ++ name)
+                      }
+                    , Cmd.none
+                    )
+
+        Err err ->
+            ( { model | error = Just err }, Cmd.none )
+
+
+msgDecoder : JD.Decoder RunnerWsMsg
+msgDecoder =
+    JDP.decode RunnerWsMsg
+        |> JDP.required "name" JD.string
+        |> JDP.required "data" runnerDecoder
+
+
+runnerDecoder : JD.Decoder Runner
+runnerDecoder =
+    JDP.decode Runner
+        |> JDP.required "_id" JD.string
+        |> JDP.required "name" JD.string
+        |> JDP.required "location" JD.string
+        |> JDP.required "age" JD.int
+        |> JDP.required "bib" JD.int
+        |> JDP.hardcoded 0
+        |> JDP.required "lastMarkerDistance" JD.float
+        |> JDP.required "lastMarkerTime" JD.float
+        |> JDP.required "pace" JD.float
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    div [ class "main" ]
+        [ errorPanel model.error
+        , searchForm model.query
+        , runners model
+        ]
+
+
+errorPanel : Maybe String -> Html a
+errorPanel error =
+    case error of
+        Nothing ->
+            text ""
+
+        Just msg ->
+            div [ class "error" ]
+                [ text msg
+                , button [ type_ "button" ] [ text "×" ]
+                ]
+
+
+searchForm : String -> Html Msg
+searchForm query =
+    Html.form [ onSubmit Search ]
+        [ input
+            [ type_ "text"
+            , placeholder "Search for runner..."
+            , value query
+            , onInput SearchInput
+            ]
+            []
+        , button [ type_ "submit" ] [ text "Search" ]
+        ]
+
+
+runners : Model -> Html Msg
+runners { query, runners } =
+    runners
+        |> List.map runner
+        |> tbody []
+        |> (\r -> runnersHeader :: [ r ])
+        |> table []
+
+
+runner : Runner -> Html Msg
+runner { name, location, age, bib, estimatedDistance } =
+    tr []
+        [ td [] [ text name ]
+        , td [] [ text location ]
+        , td [] [ text (toString age) ]
+        , td [] [ text (toString bib) ]
+        , td []
+            [ text "1 mi @ 08:30AM (TODO)"
+            ]
+        , td [] [ text (toString estimatedDistance) ]
+        ]
+
+
+runnersHeader : Html Msg
+runnersHeader =
+    thead []
+        [ tr []
+            [ th [] [ text "Name" ]
+            , th [] [ text "From" ]
+            , th [] [ text "Age" ]
+            , th [] [ text "Bib #" ]
+            , th [] [ text "Last Marker" ]
+            , th [] [ text "Est. Miles" ]
+            ]
+        ]
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    listen url WsMessage
---
+++ 14RLBWebsocket/src/Main.elm
@@ -0,0 +1,332 @@
+port module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Navigation
+import LeaderBoard
+import Login
+import Runner
+
+
+-- model
+
+
+type alias Model =
+    { page : Page
+    , leaderBoard : LeaderBoard.Model
+    , login : Login.Model
+    , runner : Runner.Model
+    , token : Maybe String
+    , loggedIn : Bool
+    }
+
+
+type Page
+    = NotFound
+    | LeaderBoardPage
+    | LoginPage
+    | RunnerPage
+
+
+authPages : List Page
+authPages =
+    [ RunnerPage ]
+
+
+init : Flags -> Navigation.Location -> ( Model, Cmd Msg )
+init flags location =
+    let
+        page =
+            hashToPage location.hash
+
+        loggedIn =
+            flags.token /= Nothing
+
+        ( updatedPage, cmd ) =
+            authRedirect page loggedIn
+
+        ( leaderboardInitModel, leaderboardCmd ) =
+            LeaderBoard.init
+
+        ( loginInitModel, loginCmd ) =
+            Login.init
+
+        ( runnerInitModel, runnerCmd ) =
+            Runner.init
+
+        initModel =
+            { page = updatedPage
+            , leaderBoard = leaderboardInitModel
+            , login = loginInitModel
+            , runner = runnerInitModel
+            , token = flags.token
+            , loggedIn = loggedIn
+            }
+
+        cmds =
+            Cmd.batch
+                [ Cmd.map LeaderBoardMsg leaderboardCmd
+                , Cmd.map LoginMsg loginCmd
+                , Cmd.map RunnerMsg runnerCmd
+                , cmd
+                ]
+    in
+        ( initModel, cmds )
+
+
+
+-- update
+
+
+type Msg
+    = Navigate Page
+    | ChangePage Page
+    | LeaderBoardMsg LeaderBoard.Msg
+    | LoginMsg Login.Msg
+    | RunnerMsg Runner.Msg
+    | Logout
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Navigate page ->
+            ( model, Navigation.newUrl <| pageToHash page )
+
+        ChangePage page ->
+            let
+                -- Is page ok for user? or Redirect to login?
+                ( updatedPage, cmd ) =
+                    authRedirect page model.loggedIn
+            in
+                ( { model | page = updatedPage }, cmd )
+
+        LeaderBoardMsg msg ->
+            let
+                ( leaderBoardModel, cmd ) =
+                    LeaderBoard.update msg model.leaderBoard
+            in
+                ( { model | leaderBoard = leaderBoardModel }
+                , Cmd.map LeaderBoardMsg cmd
+                )
+
+        LoginMsg msg ->
+            let
+                ( loginModel, cmd, token ) =
+                    Login.update msg model.login
+
+                loggedIn =
+                    token /= Nothing
+
+                saveTokenCmd =
+                    case token of
+                        Just jwt ->
+                            saveToken jwt
+
+                        Nothing ->
+                            Cmd.none
+            in
+                ( { model
+                    | login = loginModel
+                    , token = token
+                    , loggedIn = loggedIn
+                  }
+                , Cmd.batch
+                    [ Cmd.map LoginMsg cmd
+                    , saveTokenCmd
+                    ]
+                )
+
+        RunnerMsg msg ->
+            let
+                ( runnerModel, cmd ) =
+                    Runner.update
+                        (Maybe.withDefault "" model.token)
+                        msg
+                        model.runner
+            in
+                ( { model | runner = runnerModel }
+                , Cmd.map RunnerMsg cmd
+                )
+
+        Logout ->
+            ( { model
+                | loggedIn = False
+                , token = Nothing
+              }
+            , Cmd.batch
+                [ deleteToken ()
+                , Navigation.modifyUrl <| pageToHash LeaderBoardPage
+                ]
+            )
+
+
+authForPage : Page -> Bool -> Bool
+authForPage page loggedIn =
+    loggedIn || not (List.member page authPages)
+
+
+authRedirect : Page -> Bool -> ( Page, Cmd Msg )
+authRedirect page loggedIn =
+    if authForPage page loggedIn then
+        ( page, Cmd.none )
+    else
+        ( LoginPage, Navigation.modifyUrl <| pageToHash LoginPage )
+
+
+
+-- view
+
+
+view : Model -> Html Msg
+view model =
+    let
+        page =
+            case model.page of
+                LeaderBoardPage ->
+                    Html.map LeaderBoardMsg
+                        (LeaderBoard.view model.leaderBoard)
+
+                LoginPage ->
+                    Html.map LoginMsg
+                        (Login.view model.login)
+
+                RunnerPage ->
+                    Html.map RunnerMsg
+                        (Runner.view model.runner)
+
+                NotFound ->
+                    div [ class "main" ]
+                        [ h1 []
+                            [ text "Page Not Found!" ]
+                        ]
+    in
+        div []
+            [ pageHeader model
+            , page
+            ]
+
+
+authHeaderView : Model -> Html Msg
+authHeaderView model =
+    if model.loggedIn then
+        a [ onClick Logout ]
+            [ text "Logout"
+            ]
+    else
+        a [ onClick (Navigate LoginPage) ]
+            [ text "Login"
+            ]
+
+
+addRunnerLinkView : Model -> Html Msg
+addRunnerLinkView { loggedIn } =
+    if loggedIn then
+        a [ onClick (Navigate RunnerPage) ] [ text "Add Runner" ]
+    else
+        text ""
+
+
+pageHeader : Model -> Html Msg
+pageHeader model =
+    header []
+        [ a [ onClick (Navigate LeaderBoardPage) ]
+            [ text "Race Results" ]
+        , ul []
+            [ li []
+                [ addRunnerLinkView model
+                ]
+            ]
+        , ul []
+            [ li []
+                [ authHeaderView model
+                ]
+            ]
+        ]
+
+
+
+-- subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    let
+        leaderBoardSub =
+            LeaderBoard.subscriptions model.leaderBoard
+
+        loginSub =
+            Login.subscriptions model.login
+
+        runnerSub =
+            Runner.subscriptions model.runner
+    in
+        Sub.batch
+            [ Sub.map LeaderBoardMsg leaderBoardSub
+            , Sub.map LoginMsg loginSub
+            , Sub.map RunnerMsg runnerSub
+            ]
+
+
+hashToPage : String -> Page
+hashToPage hash =
+    case hash of
+        "#/" ->
+            LeaderBoardPage
+
+        "" ->
+            LeaderBoardPage
+
+        "#/login" ->
+            LoginPage
+
+        "#/add" ->
+            RunnerPage
+
+        _ ->
+            NotFound
+
+
+pageToHash : Page -> String
+pageToHash page =
+    case page of
+        LeaderBoardPage ->
+            "#/"
+
+        LoginPage ->
+            "#/login"
+
+        RunnerPage ->
+            "#/add"
+
+        NotFound ->
+            "#notfound"
+
+
+locationToMsg : Navigation.Location -> Msg
+locationToMsg location =
+    location.hash
+        |> hashToPage
+        |> ChangePage
+
+
+type alias Flags =
+    { token : Maybe String
+    }
+
+
+main : Program Flags Model Msg
+main =
+    Navigation.programWithFlags locationToMsg
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+port saveToken : String -> Cmd msg
+
+
+port deleteToken : () -> Cmd msg
