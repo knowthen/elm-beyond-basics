b8e9ea2b5077c40d2449fd3fd8bed9cd456898f2
James Moore <moorej@nulaid.com>
2016-11-28 10:39:33 -0800

    clean up server.js, fix for windows & README

--- RLBServer/README.md
+++ RLBServer/README.md
@@ -1,1 +1,22 @@
-# TODO..
+# Node Server
+
+## Setup 
+
+1. Install Recent Version of **Nodejs** by downloading and running the installer found at https://nodejs.org/en/ or use the Node Version Manager found at https://github.com/creationix/nvm
+2. Install Nodejs dependancies by keying `npm install` from within this directory
+
+## Starting Server
+
+The server can be started by keying `node server.js`
+
+## Where's the data stored?
+
+The runner records are saved into `local.db` file, via [nedb](https://github.com/louischatriot/nedb).  You can manually edit/delete this file and restart the server.
+
+## Where's the username and password?
+
+The app isn't doing real auth... The username and password is stored in the `user` file and it's loaded using [dotenv](https://github.com/motdotla/dotenv)
+
+## Bugs
+
+Yes, there are bugs in the app... Covering every edge case scenario wouldn't make for a good training videos.. We'd have to go off on too many tangents (edge cases), which would be distracting from the main content.
--- RLBServer/server.js
+++ RLBServer/server.js
@@ -17,7 +17,7 @@ const Datastore = require('nedb');
 const random = require('random-js')();
 const PubSub = require('pubsub-js');
 const db = new Datastore(
-  { filename: path.join(__dirname, 'local.db'),
+  {filename: path.join(__dirname, 'local.db'),
     autoload: true,
   });
 require('dotenv').config({path: 'user'});
@@ -39,7 +39,6 @@ const RUNNERUPDATE = 'runner update';
 const LISTENRUNNERS = 'listen runners';
 const NEWRUNNER = 'new runner';
 const UPDATERUNNER = 'update runner';
-const REMOVERUNNER = 'remove runner';
 const JWT_PW = 'secret';
 
 
@@ -53,7 +52,6 @@ const RUNNERSCHEMA = Joi.object().keys({
   lastMarkerTime: Joi.number().min(0).required(),
   pace: Joi.number().min(0).required(),
 });
-
 
 
 app.use(koaBody());
@@ -76,53 +74,53 @@ function getToken(header) {
 
 function* isAuth(next) {
   const token = getToken(this.request.get('Authorization'));
-  const { valid, err } = verifyToken(JWT_PW, token);
+  const {valid} = verifyToken(JWT_PW, token);
   if (valid) {
     yield next;
   } else {
-    this.body = "Invalid Token, Must Login Again!";
+    this.body = 'Invalid Token, Must Login Again!';
     this.status = 401;
   }
 }
 
 function newRunner(body) {
   const pace = random.real(0.08, 0.13);
-  const runner = Object.assign({ lastMarkerDistance: 0.0, lastMarkerTime: 0.0, pace },
+  const runner = Object.assign({
+    lastMarkerDistance: 0.0,
+    lastMarkerTime: 0.0,
+    pace},
       _.pick(body, ['name', 'location', 'age', 'bib']));
   return runner;
 }
 
 function* addRunner() {
-  // const pace = random.real(0.08, 0.13);
-  // const runner = Object.assign({ lastMarkerDistance: 0.0, lastMarkerTime: 0.0, pace },
-  //     _.pick(JSON.parse(this.request.body), ['name', 'location', 'age', 'bib']));
   const runner = newRunner(this.request.body);
-  const { error } = Joi.validate(runner, RUNNERSCHEMA);
+  const {error} = Joi.validate(runner, RUNNERSCHEMA);
   if (error) {
     this.body = joiValidationErrorMsg(error);
     this.status = 422;
     return;
-    // return this.throw(joiValidationErrorMsg(error), 400);
   }
   try {
     const result = yield db.insert(runner);
-    const msg = { name: NEWRUNNER, msg: result };
+    const msg = {name: NEWRUNNER, msg: result};
     PubSub.publish(RUNNERUPDATE, msg);
     this.status = 201;
     this.set('location', result._id);
     this.body = result;
   } catch (err) {
-    this.throw(err, 400);
+    this.body = 'Error saving the runner!';
+    this.status = 400;
   }
 }
 
 function* authenticate() {
-  const { username, password } = this.request.body;
-  if (username === process.env.USERNAME && password === process.env.PASSWORD) {
-    const token = jwt.sign({ id: 1, exp: ms('1 year') }, JWT_PW);
-    this.body = { token };
+  const {username, password} = this.request.body;
+  if (username === process.env.APP_USERNAME &&
+      password === process.env.APP_PASSWORD) {
+    const token = jwt.sign({id: 1, exp: ms('1 year')}, JWT_PW);
+    this.body = {token};
   } else {
-    // this.throw('Invalid Username Or Password!', 401);
     this.body = 'Invalid Username Or Password!';
     this.status = 401;
   }
@@ -132,16 +130,16 @@ wsApi.get('/runners', function* middleware() {
   const tokens = [];
   this.websocket.on('message', (message) => {
     const msg = JSON.parse(message);
-    if (msg.name === LISTENRUNNERS){
+    if (msg.name === LISTENRUNNERS) {
       const token = listenRunners(this.websocket, msg);
       tokens.push(token);
     }
   });
 
   this.websocket.on('close', () => {
-    tokens.forEach( token => {
+    tokens.forEach( (token) => {
       PubSub.unsubscribe(token);
-    })
+    });
   });
 });
 
@@ -154,29 +152,29 @@ function formatMessage(envelope, data) {
 function sendError(ws, name, error) {
   ws.send(
     formatMessage(
-      { name, id },
-      { data: error })
+      {name, id},
+      {data: error})
   );
 }
 
 function sendSuccess(ws, name, data) {
   ws.send(
     formatMessage(
-      { name},
-      { data }
+      {name},
+      {data}
     )
   );
 }
 
 function verifyToken(password, token) {
   if (!token) {
-    return { valid: false, error: 'Security Token not provided' };
+    return {valid: false, error: 'Security Token not provided'};
   } else {
     try {
       jwt.verify(token, password);
-      return { valid: true, error: null };
+      return {valid: true, error: null};
     } catch (e) {
-      return { valid: false, error: e };
+      return {valid: false, error: e};
     }
   }
 }
@@ -190,14 +188,14 @@ function joiValidationErrorMsg(error) {
 
 function listenRunners(ws, msg) {
   db.find({})
-    .then( runners => {
-      runners.forEach( runner => {
-        sendSuccess(ws, NEWRUNNER , runner)
-      })
+    .then( (runners) => {
+      runners.forEach( (runner) => {
+        sendSuccess(ws, NEWRUNNER, runner);
+      });
     })
-    .catch(err => {
+    .catch( (err) => {
       sendError(ws, 'listen runners error', err);
-    })
+    });
 
   const token = PubSub.subscribe(RUNNERUPDATE, runnerUpdate(ws));
   return token;
@@ -205,7 +203,7 @@ function listenRunners(ws, msg) {
 
 const runnerUpdate = _.curry( (ws, thisName, {name, msg}) => {
   sendSuccess(ws, name, msg);
-})
+});
 
 
 app.ws.use(wsApi.routes()).use(wsApi.allowedMethods());
@@ -215,30 +213,32 @@ app.listen(5000);
 
 
 function setTime(minute) {
-  const startTime = (new Date((new Date) - minute * 60000)).getTime()
+  const startTime = (new Date((new Date) - minute * 60000)).getTime();
   db.find({})
-    .then(runners => {
-      const promises = runners.map ( runner => {
+    .then( (runners) => {
+      const promises = runners.map ( (runner) => {
         const miles = runner.pace * minute;
         const lastMarkerDistance = miles - (miles % 1);
 
         const markerMinutes = minute - (runner.pace * (miles % 1) * 60);
-        const lastMarkerTime = minute > -1 ? (new Date( startTime + markerMinutes * 60000)).getTime() : 0;
-        const updatedRunner = Object.assign(runner, {lastMarkerDistance, lastMarkerTime});
+        const lastMarkerTime = minute > -1 ?
+          (new Date( startTime + markerMinutes * 60000)).getTime() :
+          0;
+        const updatedRunner = Object.assign(runner,
+          {lastMarkerDistance, lastMarkerTime});
         PubSub.publish(RUNNERUPDATE, {name: UPDATERUNNER, msg: updatedRunner});
         return db.update({_id: runner._id}, updatedRunner);
       });
       return Promise.all(promises);
     })
-    .catch(err => {
+    .catch( (err) => {
       console.log(err);
-    })
-
+    });
 }
 
 function replEval(cmd, context, filename, callback) {
   const args = cmd.trim().split(' ');
-  const data = parse(args)
+  const data = parse(args);
   if ('minute' in data) {
     setTime(data.minute);
     return callback(null);
@@ -255,7 +255,7 @@ function replEval(cmd, context, filename, callback) {
     process.exit();
   }
   else {
-    return callback(null, REPLHELP)
+    return callback(null, REPLHELP);
   }
 }
 console.log(REPLHELP);
